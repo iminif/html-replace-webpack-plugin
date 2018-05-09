703fad857f7727329f744b4161a9c3ca2ea76201
iminif <iminif@outlook.com>
2018-05-10 00:38:57 +0800

    Upgrade plugin

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 function HtmlReplaceWebpackPlugin(options) {
   options = Array.isArray(options) ? options : [options]
 
-  options.forEach(function(option) {
+  options.forEach(option => {
     if (typeof option.pattern == 'undefined' || typeof option.replacement == 'undefined') {
       throw new Error('Both `pattern` and `replacement` options must be defined!')
     }
   })
 
-  this.replace = function(htmlData) {
-    options.forEach(function(option) {
+  this.replace = function(htmlPluginData, callback) {
+    options.forEach(option => {
       if (typeof option.replacement === 'function') {
-        var matches = null;
-        var isPatternValid = true;
+        var matches = null
+        var isPatternValid = true
         try {
-          new RegExp(option.pattern);
+          new RegExp(option.pattern)
         } catch (e) {
-          isPatternValid = false;
+          isPatternValid = false
         }
 
-        if (!isPatternValid) throw new Error("Invalid `pattern` option provided, it must be a valid regex.");
-        while ((matches = option.pattern.exec(htmlData)) != null) {
+        if (!isPatternValid) throw new Error("Invalid `pattern` option provided, it must be a valid regex.")
+        while ((matches = option.pattern.exec(htmlPluginData.html)) != null) {
           var replacement = option.replacement.apply(null, matches)
 
           // matches[0]: matching content string
-          htmlData = htmlData.replace(matches[0], replacement)
+          htmlPluginData.html = htmlPluginData.html.replace(matches[0], replacement)
         }
       } else {
-        // htmlData = htmlData.replace(option.pattern, option.replacement)
-        htmlData = htmlData.split(option.pattern).join(option.replacement)
+        // htmlPluginData.html.replace(option.pattern, option.replacement)
+        htmlPluginData.html = htmlPluginData.html.split(option.pattern).join(option.replacement)
       }
     })
-    return htmlData
+
+    callback(null, htmlPluginData)
   }
 }
 
 HtmlReplaceWebpackPlugin.prototype.apply = function(compiler) {
-  var _this = this
-  compiler.hooks.compilation.tap('HtmlReplaceWebpackPlugin', function(compilation) {
-    // console.log('The compiler is starting a new compilation...')
-    compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('html-webpack-plugin-before-html-processing',
-      function(htmlPluginData, callback) {
-        htmlPluginData.html = _this.replace(htmlPluginData.html)
-        callback(null, htmlPluginData)
-      })
-  })
+  if (compiler.hooks) {
+    compiler.hooks.compilation.tap('HtmlReplaceWebpackPlugin', compilation => {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync('html-webpack-plugin-before-html-processing', this.replace)
+    })
+  } else {
+    compiler.plugin('compilation', compilation => {
+      compilation.plugin('html-webpack-plugin-before-html-processing', this.replace)
+    })
+  }
 }
 
 module.exports = HtmlReplaceWebpackPlugin
