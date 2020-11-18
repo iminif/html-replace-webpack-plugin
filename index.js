bf8dd2e6b56e57bc6a972134eaf101626f2aef02
Nathanie <iminif@outlook.com>
2020-11-18 15:35:23 +0800

    Update code for better performances

--- index.js
+++ index.js
@@ -7,28 +7,21 @@ function HtmlReplaceWebpackPlugin(options) {
     }
   })
 
-  this.replace = function(htmlPluginData, callback) {
+  this.replace = function (htmlPluginData, callback) {
     options.forEach(option => {
       if (typeof option.replacement === 'function') {
-        var matches = null
-        var isPatternValid = true
         try {
           new RegExp(option.pattern)
         } catch (e) {
-          isPatternValid = false
+          throw new Error('Invalid `pattern` option provided, it must be a valid regex.')
         }
 
-        if (!isPatternValid) throw new Error('Invalid `pattern` option provided, it must be a valid regex.')
-        while ((matches = option.pattern.exec(htmlPluginData.html)) != null) {
-          var replacement = option.replacement.apply(null, matches)
-
-          // matches[0]: matching content string
-          htmlPluginData.html = htmlPluginData.html.replace(matches[0], replacement)
-        }
+        htmlPluginData.html = htmlPluginData.html.replace(option.pattern, option.replacement)
       } else {
         if (option.pattern instanceof RegExp)
-            htmlPluginData.html = htmlPluginData.html.replace(option.pattern, option.replacement)
-        else htmlPluginData.html = htmlPluginData.html.split(option.pattern).join(option.replacement)
+          htmlPluginData.html = htmlPluginData.html.replace(option.pattern, option.replacement)
+        else
+          htmlPluginData.html = htmlPluginData.html.split(option.pattern).join(option.replacement)
       }
     })
 
@@ -36,19 +29,27 @@ function HtmlReplaceWebpackPlugin(options) {
   }
 }
 
-HtmlReplaceWebpackPlugin.prototype.apply = function(compiler) {
+HtmlReplaceWebpackPlugin.prototype.apply = function (compiler) {
   if (compiler.hooks) {
     compiler.hooks.compilation.tap('HtmlReplaceWebpackPlugin', compilation => {
       if (compilation.hooks.htmlWebpackPluginAfterHtmlProcessing) {
-        compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync('HtmlReplaceWebpackPlugin', this.replace)
+        compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
+          'HtmlReplaceWebpackPlugin',
+          this.replace
+        )
       } else {
         var HtmlWebpackPlugin = require('html-webpack-plugin')
 
         if (!HtmlWebpackPlugin) {
-          throw new Error('Please ensure that `html-webpack-plugin` was placed before `html-replace-webpack-plugin` in your Webpack config if you were working with Webpack 4.x!')
+          throw new Error(
+            'Please ensure that `html-webpack-plugin` was placed before `html-replace-webpack-plugin` in your Webpack config if you were working with Webpack 4.x!'
+          )
         }
 
-        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('HtmlReplaceWebpackPlugin', this.replace)
+        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+          'HtmlReplaceWebpackPlugin',
+          this.replace
+        )
       }
     })
   } else {
